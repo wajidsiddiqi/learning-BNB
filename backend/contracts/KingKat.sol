74d4b3c3953d8c242ba9c2e42a6e9e5499399e03
wajid <wajidsiddiqi12@gmail.com>
2023-10-25 21:07:01 +0500

    developing smart contract

---
+++ backend/contracts/KingKat.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+/**@author Wajid*/
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+error FloraPixel__MintNotEnabled();
+error FloraPixel__NotEnoughMoneySent();
+error FloraPixel__WeSoldOut();
+error FloraPixel__WrongId();
+
+contract FloraPixel is ERC721, Ownable {
+    using Strings for uint256;
+    //* State Variables
+    uint256 private constant MINT_PRICE = 0.01 ether;
+    uint256 private s_totalSupply = 0;
+    uint256 private constant MAX_SUPPLY = 10;
+    uint256 private s_mintState = 1;
+    string private constant BASE_TOKEN_URI =
+        "ipfs://bafybeidlnjv7bbart3azzizjh76ywpvtns67nz3c2pdu5xvytdrtwbeopu/";
+
+    //*Functions
+    constructor() ERC721("FloraPixel", "FP") {}
+
+    /**@dev This function changes the state of nft*/
+    function changeMintState() external onlyOwner {
+        if (s_mintState != 1) s_mintState = 1;
+        else s_mintState = 2;
+    }
+
+    /**@dev This is a public mint function that checks public requirements*/
+    function Mint(uint256 quantity) public payable {
+        if (s_mintState == 1) revert FloraPixel__MintNotEnabled();
+        if (msg.value != MINT_PRICE * quantity)
+            revert FloraPixel__NotEnoughMoneySent();
+
+        internalMint(quantity);
+    }
+
+    /**@dev This is a mint function and module that mint functions uses*/
+    function internalMint(uint256 quantity) internal {
+        if (s_totalSupply + quantity > MAX_SUPPLY) revert KingKat__WeSoldOut();
+
+        for (uint256 i = 0; i < quantity; i++) {
+            uint256 tokenId = s_totalSupply + 1;
+            s_totalSupply++;
+            _safeMint(msg.sender, tokenId);
+        }
+    }
+
+    /**@dev This is a tokenURI generator function*/
+    function tokenURI(
+        uint256 tokenId
+    ) public view override returns (string memory) {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory baseURI = BASE_TOKEN_URI;
+
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
+                : "";
+    }
+
+    /**@dev This is a withdraw function*/
+    function withdraw() external onlyOwner {
+        uint256 ammount = address(this).balance;
+        payable(msg.sender).transfer(ammount);
+    }
+
+    //*Getter Functions
+    function getMintState() public view returns (bool) {
+        if (s_mintState != 1) return true;
+        else return false;
+    }
+
+    function getBaseTokenURI() public pure returns (string memory) {
+        return BASE_TOKEN_URI;
+    }
+
+    function getTotalSupply() public view returns (uint256) {
+        return s_totalSupply;
+    }
+
+    function getMaxSupply() public pure returns (uint256) {
+        return MAX_SUPPLY;
+    }
+
+    function getMintPrice() public view returns (uint256) {
+        return MINT_PRICE;
+    }
+}
