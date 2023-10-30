1a94f605b6d78d20f4c879a045fa72d7d4526104
wajid <wajidsiddiqi12@gmail.com>
2023-10-30 16:20:22 +0500

    testing nft contract

--- backend/test/unit/florapixel.test.js
+++ backend/test/unit/florapixel.test.js
@@ -4,39 +4,50 @@ const { assert, expect } = require("chai");
 
 !developmentChains.includes(network.name)
   ? describe.skip
-  : describe("king Kat Nft Unit Tests", function () {
-      let kingKat, deployer, kingKatDeployed;
+  : describe("Flora Pixel Nft Unit Tests", function () {
+      let floraPixel, deployer, floraPixelDeployed;
 
       beforeEach(async () => {
         accounts = await ethers.getSigners();
         deployer = accounts[0];
-        await deployments.fixture(["kingKat"]);
-        kingKatDeployed = await deployments.get("KingKat");
-        kingKat = await ethers.getContractAt(
-          "KingKat",
-          kingKatDeployed.address
+        await deployments.fixture(["floraPixel"]);
+        floraPixelDeployed = await deployments.get("FloraPixel");
+        floraPixel = await ethers.getContractAt(
+          "FloraPixel",
+          floraPixelDeployed.address
         );
       });
 
       describe("Constructor", () => {
         it("Initializes the NFT Correctly.", async () => {
-          const mintState = await kingKat.getMintState();
-          const price = await kingKat.getPrice();
-          const maxSupply = await kingKat.getMaxSupply();
-          const id = await kingKat.getTokenAId();
+          const name = await floraPixel.name();
+          const symbol = await floraPixel.symbol();
+          const owner = await floraPixel.owner();
+          const mintState = await floraPixel.getMintState();
+          const price = await floraPixel.getMintPrice();
+          const maxSupply = await floraPixel.getMaxSupply();
+          const totalSupply = await floraPixel.getTotalSupply();
+          const baseTokenURI = await floraPixel.getBaseTokenURI();
 
+          assert.equal(name, "FloraPixel");
+          assert.equal(symbol, "FP");
+          assert.equal(owner, "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
           assert.equal(mintState, false);
           assert.equal(price.toString(), ethers.parseEther("0.01"));
-          assert.equal(maxSupply, 15);
-          assert.equal(id, 1);
+          assert.equal(maxSupply, 12);
+          assert.equal(totalSupply, 2);
+          assert.equal(
+            baseTokenURI,
+            "ipfs://bafybeih5lhmwj4rz74dgpznvr2dsofwibklyo3aumbzf2oz7aqlhpiv22y/"
+          );
         });
       });
 
       describe("Mint State Change", () => {
         it("changes mint state", async () => {
-          const txResponse = await kingKat.changeMintState();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
-          const mintState = await kingKat.getMintState();
+          const mintState = await floraPixel.getMintState();
 
           assert.equal(mintState, true);
         });
@@ -46,49 +57,55 @@ const { assert, expect } = require("chai");
           const nonowner = accounts[1];
 
           await expect(
-            kingKat.connect(nonowner).changeMintState()
-          ).to.be.revertedWith("Ownable: caller is not the owner");
+            floraPixel.connect(nonowner).changeMintState()
+          ).to.be.revertedWithCustomError(
+            floraPixel,
+            "OwnableUnauthorizedAccount"
+          );
         });
       });
-
+      /*
       describe("Mint", () => {
         let quantity, value, id;
 
         beforeEach(async () => {
-          const price = await kingKat.getPrice();
+          const price = await floraPixel.getPrice();
           quantity = 1;
           value = BigInt(price.toString()) * BigInt(quantity);
-          id = await kingKat.getTokenAId();
+          id = await floraPixel.getTokenAId();
         });
 
         it("reverts if mint is not open", async () => {
           await expect(
-            kingKat.mint(id, quantity, { value: value })
-          ).to.be.revertedWithCustomError(kingKat, "KingKat__MintNotEnabled");
+            floraPixel.mint(id, quantity, { value: value })
+          ).to.be.revertedWithCustomError(
+            floraPixel,
+            "floraPixel__MintNotEnabled"
+          );
         });
 
         it("reverts if wrong mint value added", async () => {
-          const txResponse = await kingKat.changeMintState();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
 
           await expect(
-            kingKat.mint(id, 2, { value: value })
+            floraPixel.mint(id, 2, { value: value })
           ).to.be.revertedWithCustomError(
-            kingKat,
-            "KingKat__NotEnoughMoneySent"
+            floraPixel,
+            "floraPixel__NotEnoughMoneySent"
           );
         });
 
         it("reverts if we sold out", async () => {
-          const txResponse = await kingKat.changeMintState();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
 
           const accounts = await ethers.getSigners();
-          const maxSupply = await kingKat.getMaxSupply();
+          const maxSupply = await floraPixel.getMaxSupply();
 
           for (let i = 0; i < maxSupply; i++) {
             const mintAccounts = accounts[i];
-            const txResponse = await kingKat
+            const txResponse = await floraPixel
               .connect(mintAccounts)
               .mint(id, quantity, {
                 value: value,
@@ -97,29 +114,29 @@ const { assert, expect } = require("chai");
           }
 
           await expect(
-            kingKat.mint(id, quantity, { value: value })
-          ).to.be.revertedWithCustomError(kingKat, "KingKat__WeSoldOut");
+            floraPixel.mint(id, quantity, { value: value })
+          ).to.be.revertedWithCustomError(floraPixel, "floraPixel__WeSoldOut");
         });
 
         it("reverts if wrong token id added", async () => {
-          const txResponse = await kingKat.changeMintState();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
 
           await expect(
-            kingKat.mint(2, quantity, { value: value })
-          ).to.be.revertedWithCustomError(kingKat, "KingKat__WrongId");
+            floraPixel.mint(2, quantity, { value: value })
+          ).to.be.revertedWithCustomError(floraPixel, "floraPixel__WrongId");
         });
 
         it("mints nft and updates accordingly", async () => {
-          const txResponse1 = await kingKat.changeMintState();
+          const txResponse1 = await floraPixel.changeMintState();
           await txResponse1.wait(1);
-          const txResponse = await kingKat.mint(id, quantity, {
+          const txResponse = await floraPixel.mint(id, quantity, {
             value: value,
           });
           await txResponse.wait(1);
-          const totalSupply = await kingKat.totalSupply(id);
+          const totalSupply = await floraPixel.totalSupply(id);
           const userAddress = deployer.address;
-          const userBalance = await kingKat.balanceOf(userAddress, id);
+          const userBalance = await floraPixel.balanceOf(userAddress, id);
 
           assert.equal(totalSupply, 1);
           assert.equal(userBalance, 1);
@@ -130,27 +147,27 @@ const { assert, expect } = require("chai");
         let quantity, value, id;
 
         beforeEach(async () => {
-          const price = await kingKat.getPrice();
+          const price = await floraPixel.getPrice();
           quantity = 1;
           value = BigInt(price.toString()) * BigInt(quantity);
-          id = await kingKat.getTokenAId();
-          const txResponse = await kingKat.changeMintState();
+          id = await floraPixel.getTokenAId();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
         });
 
         it("reverts if id don't exist", async () => {
-          await expect(kingKat.uri(id)).to.be.revertedWithCustomError(
-            kingKat,
-            "KingKat__TokenNonexistent"
+          await expect(floraPixel.uri(id)).to.be.revertedWithCustomError(
+            floraPixel,
+            "floraPixel__TokenNonexistent"
           );
         });
 
         it("returns uri", async () => {
-          const txResponse = await kingKat.mint(id, quantity, {
+          const txResponse = await floraPixel.mint(id, quantity, {
             value: value,
           });
           await txResponse.wait(1);
-          const uri = await kingKat.uri(id);
+          const uri = await floraPixel.uri(id);
 
           assert.equal(
             uri.toString(),
@@ -163,15 +180,15 @@ const { assert, expect } = require("chai");
         let quantity, value, id;
 
         beforeEach(async () => {
-          const price = await kingKat.getPrice();
+          const price = await floraPixel.getPrice();
           quantity = 1;
           value = BigInt(price.toString()) * BigInt(quantity);
-          id = await kingKat.getTokenAId();
-          const txResponse = await kingKat.changeMintState();
+          id = await floraPixel.getTokenAId();
+          const txResponse = await floraPixel.changeMintState();
           await txResponse.wait(1);
 
           const accounts = await ethers.getSigners();
-          const txResponse2 = await kingKat
+          const txResponse2 = await floraPixel
             .connect(accounts[1])
             .mint(id, quantity, {
               value: value,
@@ -183,24 +200,24 @@ const { assert, expect } = require("chai");
           const accounts = await ethers.getSigners();
           const nonowner = accounts[2];
 
-          await expect(kingKat.connect(nonowner).withdraw()).to.be.revertedWith(
-            "Ownable: caller is not the owner"
-          );
+          await expect(
+            floraPixel.connect(nonowner).withdraw()
+          ).to.be.revertedWith("Ownable: caller is not the owner");
         });
 
         it("successfully withdraws", async () => {
           const initialBalance = await ethers.provider.getBalance(
-            kingKatDeployed.address
+            floraPixelDeployed.address
           );
           const initialOwnerBalance = await ethers.provider.getBalance(
             deployer.address
           );
-          const tx = await kingKat.withdraw();
+          const tx = await floraPixel.withdraw();
           const receipt = await tx.wait();
 
           const gasCost = receipt.gasUsed * tx.gasPrice;
           const finalBalance = await ethers.provider.getBalance(
-            kingKatDeployed.address
+            floraPixelDeployed.address
           );
           const finalOwnerBalance = await ethers.provider.getBalance(
             deployer.address
@@ -212,5 +229,5 @@ const { assert, expect } = require("chai");
             (initialOwnerBalance - gasCost + BigInt(initialBalance)).toString()
           );
         });
-      });
+      });*/
     });
