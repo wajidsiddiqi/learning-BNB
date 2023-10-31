2c6853b39027c9cb68cc4d233573cae9c6d4e10c
wajid <wajidsiddiqi12@gmail.com>
2023-10-31 20:51:11 +0500

    added args

--- backend/deploy/01-deploy-floraPixel.js
+++ backend/deploy/01-deploy-floraPixel.js
@@ -6,19 +6,21 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  const args = ["0xF18DC19aCff2feA64202E6db906187CAa9803Fd0"];
+
   const floraPixel = await deploy("FloraPixel", {
     from: deployer,
     log: true,
-    args: ["0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"],
+    args: args,
     waitConfirmations: network.config.blockConfirmations || 1,
   });
 
   if (
     !developmentChains.includes(network.name) &&
-    process.env.ETHERSCAN_API_KEY
+    process.env.BSCSCAN_API_KEY
   ) {
     log("Verifying.....");
-    await verify(floraPixel.address);
+    await verify(floraPixel.address, args);
   }
 
   log("---------------------------------------");
