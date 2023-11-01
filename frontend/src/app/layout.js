e3db8da9b36c45a2774818df30f0189fc5b996fc
wajid <wajidsiddiqi12@gmail.com>
2023-11-01 23:17:32 +0500

    layout

---
+++ frontend/src/app/layout.js
@@ -0,0 +1,18 @@
+import { Providers } from "./providers";
+import { sora } from "./fonts";
+import "@/app/styles/global.css";
+
+export const metadata = {
+  title: "Flora Pixel",
+  description: "Portfolio project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={sora.className}>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
